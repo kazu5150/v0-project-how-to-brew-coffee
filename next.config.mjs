323d212f294e7f7e6ddcd24f7ef34d0dbd2d238f
kazu5150 <kazu.deadtotheworld@gmail.com>
2024-09-14 21:05:30 +0900

    Configure for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/v0-project-how-to-brew-coffee-public/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/v0-project-how-to-brew-coffee-public' : '',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
+  
